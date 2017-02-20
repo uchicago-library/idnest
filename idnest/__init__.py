b7a76bebdeecea1c1ff5156df17e2b155de3c2c7
brian <balsamo@uchicago.edu>
2017-02-20 13:59:51 -0600

    Adding license, fixing failure in init if no config present.

--- idnest/__init__.py
+++ idnest/__init__.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 #app.config['MONGO_HOST'] = 'localhost'
 #app.config['MONGO_PORT'] = 27017
 #app.config['MONGO_DB'] = "dev"
-app.config.from_envvar("IDNEST_CONFIG")
+app.config.from_envvar("IDNEST_CONFIG", silent=True)
 
 app.register_blueprint(BLUEPRINT)
